cdbd892af1f96a78639ab72002d3be20e60d97c4
Dmytro Yablonskiy <dmytroyablonskiy@MacBook-Air-Dmytro.local>
2022-12-12 11:50:55 +0200

    added homework 2

---
+++ homework 2/script2.js
@@ -0,0 +1,39 @@
+let N = +prompt("Введіть початкове число");
+
+while(!Number.isInteger(N) || isNaN(N)) {
+
+alert("Потрібно ввести ціле число");
+
+N = +prompt("Введіть початкове число");
+
+};
+
+let M = +prompt("Введіть кінцеве число");
+
+while(!Number.isInteger(M) || isNaN(M)) {
+
+    alert("Потрібно ввести ціле число");
+    
+    M = +prompt("Введіть кінцеве число");
+    
+    };
+
+let numWhole = +confirm("Чи хотіли б ви пропускати парні числа");
+
+let sum = 0;
+
+let sum2 = 0;
+
+if(numWhole){
+    for (let index = N; index <= M; index++){
+        if(index % 2 != 0)
+            sum2 += index;
+       } 
+       document.writeln('<h1>',`Сума непарних чисел від ${N} до ${M}: ${sum2}`,'<h1>')
+    } else {
+        for (let index = N; index <= M; index++){
+            sum += index;   
+        }
+        document.writeln('<h1>',`Сума чисел від ${N} до ${M}: ${sum}`,'<h1>')
+       }
+   
